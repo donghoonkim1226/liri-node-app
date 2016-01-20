2d77d17c0e8f23c46cd6501e52e72675a7c9b96f
dong hoon kim <donghoonkim1226@gmail.com>
2016-01-20 18:19:58 -0500

    Got the twitter keys to work, but was unable to pull the tweets. Also getting error.

--- liri.js
+++ liri.js
@@ -1,17 +1,26 @@
 //declaring variables
 
 var fs = require("fs");
-// var keys = require("./keys.js");
-// var twitter = require('twitter');
-var spotify = require('spotify');
-var request = require('request');
+var keys = require("./keys.js");
+var twitter = require("twitter");
+var spotify = require("spotify");
+var request = require("request");
 
 var params = process.argv.slice(2);
 
+var Twitter = require("twitter");
+ 
+var client = new Twitter({
 
-// omdb - movie search
+  consumer_key: keys.twitterKeys.consumer_key,
+  consumer_secret: keys.twitterKeys.consumer_secret,
+  access_token_key: keys.twitterKeys.access_token_key,
+  access_token_secret: keys.twitterKeys.access_token_secret,
+});
+console.log(client);
 
 switch(params[0]){
+
   case"movie-this":
   userMovie();
   break;
@@ -20,13 +29,19 @@ switch(params[0]){
   userSong();
   break;
 
+  case"my-tweets":
+  userTweets();
+  break;
+
   default:
   console.log("err")
 }
 
+// Enter title of movie to get the Title, Year, IMDB Rating, Country, Language, Plot, Actor, Rotten Tomatoes Rating and Rotten Tomators URL.
+
 function userMovie(){
   if(params[1] === undefined){
-    request('http://www.omdbapi.com/?t=mr+nobody&y=&plot=short&r=json&tomatoes=true', function (error, response, body) {
+    request("http://www.omdbapi.com/?t=mr+nobody&y=&plot=short&r=json&tomatoes=true", function (error, response, body) {
       if (!error && response.statusCode == 200) {
         var movieInfo = "Title: " + JSON.parse(body)["Title"] + "\r\n" +
                         "Year: " + JSON.parse(body)["Year"] + "\r\n" +
@@ -42,7 +57,7 @@ function userMovie(){
       }
     })
   } else {
-    request('http://www.omdbapi.com/?t=' + params[1] + '&y=&plot=short&r=json&tomatoes=true', function (error, response, body) {
+    request("http://www.omdbapi.com/?t=" + params[1] + "&y=&plot=short&r=json&tomatoes=true", function (error, response, body) {
       if (!error && response.statusCode == 200) {
         var movieInfo = "Title: " + JSON.parse(body)["Title"] + "\r\n" +
                         "Year: " + JSON.parse(body)["Year"] + "\r\n" +
@@ -60,7 +75,7 @@ function userMovie(){
   }
 }
 
-// spotify
+// Enter song title to return Artist, Song Name, Album Name & 30 second preview.
 
 function userSong(){
   if(params[1] === undefined){
@@ -92,3 +107,13 @@ function userSong(){
   }
 }
 
+
+
+  function  userTweets(){
+    client.get("statuses/user_timeline", {screen_name: "donghoonnn"}, function(error, tweets, response){
+      if (error) {
+        console.log(error);
+        return;
+      } 
+    });
+  }
